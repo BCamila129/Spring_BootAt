67fd432a42663993495599e22573d67cbea791f3
BCamila129 <camilabruna129@gmail.com>
2022-11-01 22:05:09 -0300

    projeto atualizado em novo repository

---
+++ src/main/java/system/demo/repository/Repository.java
@@ -0,0 +1,12 @@
+package system.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import system.demo.entity.Cliente;
+
+public interface Repository extends JpaRepository<Cliente, Long> {
+
+
+
+
+
+}
